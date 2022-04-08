bc88619156d290aef21201cf02b5086bdcd46c69
syunhua <102355763+syunhua@users.noreply.github.com>
2022-04-08 13:10:10 +0800

    Update crawler.py

--- ptt_data/crawler.py
+++ ptt_data/crawler.py
@@ -141,7 +141,9 @@ def main( start_page, end_page):
     pages = ( int(end_page) - int(start_page) ) + 1
     date_list=[]
     for p in range(pages):
-        date_list.append(start_page + int(p))
+        date_list.append(
+            start_page +
+            int(p))
     #db_router = Router()
     for page in tqdm(date_list):
         logger.info(page)
